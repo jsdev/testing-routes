73e661cf760d79482b1b2606dda7c6cb363b89d7
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2023-07-12 13:46:50 -0400

    Create zoomLevels.js

---
+++ zoomLevels.js
@@ -0,0 +1,22 @@
+export const zoom100 = { zoom: 'zoom 100%', viewportWidth: 1899, viewportHeight: 1057 }
+export const zoom110 = { zoom: 'zoom 110%', viewportWidth: 1720, viewportHeight: 960 }
+export const zoom125 = { zoom: 'zoom 125%', viewportWidth: 1510, viewportHeight: 840 }
+export const zoom150 = { zoom: 'zoom 150%', viewportWidth: 1260, viewportHeight: 705 } 
+export const zoom175 = { zoom: 'zoom 175%', viewportWidth: 1077, viewportHeight: 604 }
+export const zoom200 = { zoom: 'zoom 200%', viewportWidth: 924, viewportHeight: 529 }
+export const zoom250 = { zoom: 'zoom 250%', viewportWidth: 752, viewportHeight: 423 }
+export const zoom300 = { zoom: 'zoom 300%', viewportWidth: 625, viewportHeight: 352 }
+export const zoom400 = { zoom: 'zoom 400%', viewportWidth: 467, viewportHeight: 264 }
+export const zoom500 = { zoom: 'zoom 500%', viewportWidth: 372, viewportHeight: 211 } 
+
+export default [
+  zoom100,
+  zoom125,
+  zoom150,
+  zoom175,
+  zoom200,
+  zoom250,
+  zoom300,
+  zoom400,
+  zoom500
+];
