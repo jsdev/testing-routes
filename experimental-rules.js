ee29b0e17d23513007fa2b598ee131daf93118f8
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2023-07-13 09:07:56 -0400

    Create experimental-rules.js

---
+++ experimental-rules.js
@@ -0,0 +1,11 @@
+export default {
+  'css-orientation-lock': { enabled: true },
+  'focus-order-semantics': { enabled: true },
+  'hidden-content': { enabled: true },
+  'label-content-name-mismatch': { enabled: true },
+  'link-in-text-block': { enabled: true },
+  'no-autoplay-audio': { enabled: true },
+  'p-as-heading': { enabled: true },
+  'table-fake-caption': { enabled: true },
+  'td-has-header': { enabled: true } 
+}
